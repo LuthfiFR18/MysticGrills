46d5122be9a8895ddbee92fdc7af0a23ffdbbaa7
MasterCrew19 <rijal.abrarr@gmail.com>
2023-12-20 10:41:57 +0700

    Create Users.java

---
+++ src/model/Users.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Users {
+
+//	Admin, chef, waiter, cashier, customer 5
+	
+    public static final String ADMIN = "Admin";
+    public static final String CHEF = "Chef";
+    public static final String WAITER = "Waiter";
+    public static final String CASHIER = "Cashier";
+    public static final String CUSTOMER = "Customer";
+
+
+    private String role;
+
+}
